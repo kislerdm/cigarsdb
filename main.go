3a6ad6676be1d3939c14d97ddba3c56427d8145f
Dmitry Kisler <admin@dkisler.com>
2025-02-07 17:22:37 +0100

    up Signed-off-by: Dmitry Kisler <admin@dkisler.com>

--- main.go
+++ main.go
@@ -8,26 +8,34 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"strconv"
+	"strings"
 	"sync"
+
+	"golang.org/x/net/html"
+	"golang.org/x/net/html/atom"
 )
 
 func main() {
 	fName := "/tmp/cigardsdb_nobelgo.json"
-	f, err := os.OpenFile(fName, os.O_CREATE|os.O_RDWR, 0774)
+	f, err := os.OpenFile(fName, os.O_CREATE|os.O_TRUNC, 0664)
 	if err != nil {
-		log.Printf("could not open file: %w", err)
+		log.Printf("could not open file: %v", err)
 		return
 	}
 	defer func() { _ = f.Close() }()
 
-	data, err := ExtractNobelgo(http.DefaultClient)
+	data, err, warn := ExtractNobelgo(http.DefaultClient)
 	if err != nil {
 		log.Println(err)
 		return
 	}
+	if warn != nil {
+		log.Printf("warnings: \n%v\n", warn)
+	}
 
 	if err = json.NewEncoder(f).Encode(data); err != nil {
-		log.Printf("could write data to %s: %w", fName, err)
+		log.Printf("could write data to %s: %v", fName, err)
 	}
 }
 
@@ -36,55 +44,67 @@ type httpClient interface {
 }
 
 type Record struct {
-	Name           string   `json:"name"`
-	URL            string   `json:"url"`
-	Manufacturer   string   `json:"manufacturer"`
-	OriginCountry  string   `json:"manufacturerCountry"`
-	Collection     string   `json:"collection"`
-	Format         string   `json:"format"`
-	Form           string   `json:"form"`
-	WrapperCountry []string `json:"wrapperCountry"`
-	FillerCountry  []string `json:"fillingCountry"`
-	BinderCountry  []string `json:"binderCountry"`
-	Strength       string   `json:"strength"`
-	Price          float64  `json:"price"`
-	Diameter       float64  `json:"diameter"`
-	Circumference  float64  `json:"circumference"`
-	Length         float64  `json:"length"`
-	Aromas         []string `json:"aromas"`
+	Name            string   `json:"name"`
+	URL             string   `json:"url"`
+	Brand           string   `json:"brand"`
+	Collection      string   `json:"collection"`
+	OriginCountry   string   `json:"manufacturerCountry"`
+	Format          string   `json:"format"`
+	Form            string   `json:"form"`
+	Blender         string   `json:"blender"`
+	FillingType     string   `json:"fillingType"`
+	WrapperType     string   `json:"wrapperType"`
+	WrapperCountry  []string `json:"wrapperCountry"`
+	FillerCountry   []string `json:"fillingCountry"`
+	BinderCountry   []string `json:"binderCountry"`
+	Strength        string   `json:"strength"`
+	Price           float64  `json:"price"`
+	Diameter        float64  `json:"diameter_mm"`
+	Gauge           int      `json:"gauge"`
+	Length          int      `json:"length_mm"`
+	Aroma           string   `json:"aroma"`
+	Aromas          []string `json:"aromas"`
+	SmokingDuration string   `json:"smokingDuration"`
+	LimitedEdition  bool     `json:"limitedEdition"`
+	include         bool
 }
 
-func ExtractNobelgo(c httpClient) (o []Record, err error) {
+func ExtractNobelgo(c httpClient) (o []Record, err error, warn error) {
 	page := 1
-	for {
-		// filters:
-		//	- the gauge: between 50 and 60
-		//  - only in stock
-		// sorted by price in ascending order
-		baseURL := fmt.Sprintf("https://www.noblego.de/zigarren/?cig_gauge%5B0%5D=218&cig_gauge%5B1%5D=217&cig_gauge%5B2%5D=216&"+"cig_gauge%5B3%5D=215&cig_gauge%5B4%5D=214&cig_gauge%5B5%5D=213&cig_gauge%5B6%5D=212&cig_gauge%5B7%5D=1025&cig_gauge%5B8%5D=211&cig_gauge%5B9%5D=210&dir=asc&in_stock=1&limit=96&order=price&p=%d", page)
-		var er error
-		if resp, er := c.Get(baseURL); er == nil {
-			if data, er := io.ReadAll(resp.Body); er == nil {
-				func() { _ = resp.Body.Close() }()
-				if v, found, er := readPage(data, c); er == nil {
-					if found {
-						o = append(o, v...)
-					} else {
-						break
-					}
+	for page < 2 {
+		log.Printf("fetch data from page %d\n", page)
+
+		baseURL := fmt.Sprintf("https://www.noblego.de/zigarren/?in_stock=1&limit=96&p=%d", page)
+		var (
+			er   error
+			resp *http.Response
+		)
+		if resp, er = c.Get(baseURL); er == nil {
+			v, found, er, warnInner := readPage(resp.Body, c)
+			if er == nil {
+				_ = resp.Body.Close()
+				if found {
+					o = append(o, v...)
+				} else {
+					break
 				}
 			}
+			warn = errors.Join(warn, warnInner)
 		}
 		if er != nil {
 			err = errors.Join(err, fmt.Errorf("error reading page %d: %w", page, er))
 		}
 		page++
 	}
-	return o, err
+	return o, err, warn
 }
 
-func readPage(v []byte, c httpClient) (o []Record, found bool, err error) {
-	o = readList(v)
+func readPage(v io.Reader, c httpClient) (o []Record, found bool, err error, warn error) {
+	o, err, warn = readList(v)
+	var flag = make(chan struct{}, len(o))
+	for range o {
+		flag <- struct{}{}
+	}
 	var mu = new(sync.Mutex)
 	for i, el := range o {
 		i := i
@@ -93,31 +113,276 @@ func readPage(v []byte, c httpClient) (o []Record, found bool, err error) {
 			var (
 				er   error
 				resp *http.Response
-				data []byte
 			)
 			if resp, er = c.Get(el.URL); er == nil {
-				defer func() { _ = resp.Body.Close() }()
-				if data, er = io.ReadAll(resp.Body); er == nil {
-					func() { _ = resp.Body.Close() }()
-					if er = readDetails(data, &el); er == nil {
-						o[i] = el
-					}
+				if er = readDetails(resp.Body, &el); er == nil {
+					o[i] = el
 				}
+				_ = resp.Body.Close()
 			}
 			if er != nil {
 				mu.Lock()
 				err = errors.Join(err, fmt.Errorf("error reading details using %s: %w", el.URL, er))
 				mu.Unlock()
 			}
+			<-flag
 		}()
 	}
-	return o, len(o) > 0, err
+	return o, len(o) > 0, err, warn
 }
 
-func readList(v []byte) []Record {
-	panic("todo")
+func readList(v io.Reader) (o []Record, err error, warn error) {
+	var (
+		cnt int
+		mu  = new(sync.Mutex)
+		wg  = new(sync.WaitGroup)
+	)
+	if doc, er := html.Parse(v); er == nil {
+		for nn := range doc.Descendants() {
+			if nn.Type == html.ElementNode && nn.DataAtom == atom.Li {
+				for _, attr := range nn.Attr {
+					if attr.Key == "class" && attr.Val == "item" {
+						log.Printf("fetching data for the item %d\n", cnt)
+
+						go func(wg *sync.WaitGroup) {
+							wg.Add(1)
+							defer wg.Done()
+							r, er := readListElement(nn)
+							mu.Lock()
+							defer mu.Unlock()
+							if er == nil {
+								o = append(o, r)
+							} else {
+								warn = errors.Join(warn, fmt.Errorf("item %d: %w", cnt, er))
+							}
+						}(wg)
+
+						cnt++
+					}
+				}
+			}
+		}
+	} else {
+		err = fmt.Errorf("could not parse the list page: %w", er)
+	}
+
+	wg.Wait()
+	return o, err, warn
 }
 
-func readDetails(data []byte, o *Record) error {
-	panic("todo")
+func readListElement(n *html.Node) (r Record, warn error) {
+	var extractedBaseInfo, extractedAttrs, extractedPrice bool
+	for nn := range n.Descendants() {
+		if extractedBaseInfo && extractedPrice && extractedAttrs {
+			break
+		}
+
+		if nn.DataAtom == atom.Div {
+			for _, att := range nn.Attr {
+				if att.Key == "class" {
+					switch {
+					case strings.Contains(att.Val, "product-item-info"):
+						readListProductBaseInfo(nn, &r)
+						extractedBaseInfo = true
+					case att.Val == "product-attributes":
+						readListProductDetails(nn, &r)
+						extractedAttrs = true
+					case att.Val == "add-to-cart":
+						// the price indicates if the data point shall be skipped
+						readListProductPrice(nn, &r)
+						extractedPrice = true
+					}
+				}
+			}
+		}
+	}
+
+	// include the entries which prices are indicated per box
+	// it's often an indication of special offers, or items beyond the scope of the database, e.g., etui/cases etc.
+	if !r.include {
+		r = Record{}
+		warn = fmt.Errorf("no required data found")
+	}
+	return r, warn
+}
+
+func readListProductPrice(n *html.Node, r *Record) {
+	var c *html.Node
+	for nn := range n.Descendants() {
+		if r.include {
+			break
+		}
+		if nn.DataAtom == atom.Ul {
+			for _, att := range nn.Attr {
+				if att.Key == "class" && att.Val == "product-prices" {
+					for nnn := range nn.ChildNodes() {
+						for nnnn := range nnn.Descendants() {
+							if nnnn.DataAtom == atom.Span {
+								for _, att := range nnnn.Attr {
+									if att.Key == "title" && att.Val == "Verpackungseinheit" &&
+										nnnn.LastChild.Data == "Einzeln" {
+										r.include = true
+										c = nnn
+									}
+								}
+							}
+						}
+					}
+				}
+			}
+		}
+	}
+	if r.include {
+		for nnn := range c.Descendants() {
+			if nnn.DataAtom == atom.Span {
+				for _, att := range nnn.Attr {
+					if att.Key == "class" && att.Val == "price" {
+						tmp := nnn.LastChild.Data
+						// remove length of euro sign with the unbreakable space
+						tmp = tmp[:len(tmp)-5]
+						tmp = strings.Replace(tmp, ",", ".", -1)
+						r.Price, _ = strconv.ParseFloat(tmp, 64)
+					}
+				}
+			}
+		}
+	}
+}
+
+func readListProductDetails(n *html.Node, r *Record) {
+	spanReader := func(n *html.Node, fn func(n *html.Node) string) (o string) {
+		for c := range n.ChildNodes() {
+			for _, att := range c.Attr {
+				if att.Key == "class" && att.Val == "data" {
+					o = fn(c)
+					break
+				}
+			}
+		}
+		return o
+	}
+
+	dataFromFirstSpanChild := func(n *html.Node) string {
+		return spanReader(n, func(n *html.Node) string { return n.FirstChild.Data })
+	}
+	dataFromATagText := func(n *html.Node) string {
+		return spanReader(n, func(n *html.Node) string {
+			for nnn := range n.Descendants() {
+				if nnn.DataAtom == atom.A {
+					return nnn.LastChild.Data
+				}
+			}
+			return ""
+		})
+	}
+
+	for nn := range n.Descendants() {
+		if nn.DataAtom == atom.Li {
+			for _, att := range nn.Attr {
+				if att.Key == "class" {
+					switch att.Val {
+					case "product-attribute-herkunft":
+						r.OriginCountry = dataFromATagText(nn)
+
+					case "product-attribute-cig_size":
+						r.Format = dataFromATagText(nn)
+
+					case "product-attribute-cig_wrapper_origin":
+						r.WrapperCountry = parseConcatSlice(dataFromFirstSpanChild(nn))
+
+					case "product-attribute-cig_filler":
+						r.FillerCountry = parseConcatSlice(dataFromFirstSpanChild(nn))
+
+					case "product-attribute-cig_aroma":
+						r.Aromas = parseConcatSlice(dataFromFirstSpanChild(nn))
+
+					case "product-attribute-cig_form":
+						r.Form = dataFromATagText(nn)
+
+					}
+				}
+			}
+		}
+	}
+}
+
+func parseConcatSlice(s string) []string {
+	o := strings.Split(s, ",")
+	for i, v := range o {
+		o[i] = strings.TrimSpace(v)
+	}
+	return o
+}
+
+func readListProductBaseInfo(n *html.Node, r *Record) {
+	for nn := range n.Descendants() {
+		switch nn.DataAtom {
+		case atom.H2:
+			for _, att := range nn.Attr {
+				if att.Key == "class" && att.Val == "product-name" {
+					readUrlAndName(nn, r)
+				}
+			}
+
+		case atom.Span:
+			for _, att := range nn.Attr {
+				if att.Key == "class" {
+					switch att.Val {
+					case "product-attribute-cig_diameter":
+						v := readSpanChildrenValue(nn)
+						r.Diameter, _ = strconv.ParseFloat(v, 64)
+					case "product-attribute-cig_length":
+						v := readSpanChildrenValue(nn)
+						r.Length, _ = strconv.Atoi(v)
+					case "product-attribute-strength":
+						r.Strength = readSpanChildrenValue(nn)
+					}
+				}
+			}
+		}
+	}
+}
+
+func readSpanChildrenValue(n *html.Node) string {
+	var s string
+	for nn := range n.Descendants() {
+		if nn.DataAtom == atom.Span {
+			for _, att := range nn.Attr {
+				if att.Key == "class" && att.Val == "value" {
+					s = nn.FirstChild.Data
+				}
+			}
+		}
+	}
+	return s
+}
+
+func readUrlAndName(n *html.Node, r *Record) {
+	for nn := range n.Descendants() {
+		if nn.DataAtom == atom.A {
+			for _, at := range nn.Attr {
+				switch at.Key {
+				case "href":
+					r.URL = at.Val
+				case "title":
+					r.Name = at.Val
+				}
+			}
+		}
+	}
+}
+
+func readDetails(v io.Reader, o *Record) (err error) {
+	if doc, er := html.Parse(v); er == nil {
+		for n := range doc.Descendants() {
+			if n.DataAtom == atom.Div {
+				for _, att := range n.Attr {
+					if att.Key == "class" && strings.HasPrefix(att.Val, "product-secondary-column") {
+
+					}
+				}
+			}
+		}
+	}
+	return err
 }
--- main_test.go
+++ main_test.go
@@ -1,10 +1,13 @@
 package main
 
 import (
+	"bytes"
 	_ "embed"
+	"strings"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+	"golang.org/x/net/html"
 )
 
 // the html contain the list with three items, two of which correspond to a single unit
@@ -15,34 +18,196 @@ var list []byte
 func Test_readList(t *testing.T) {
 	want := []Record{
 		{
-			Name:           "Reposado Estate Blend Colorado Robusto",
-			URL:            "https://www.noblego.de/reposado-estate-blend-colorado-robusto-zigarren/",
+			Name:           "Diesel Crucible Limited Edition 2021 Toro",
+			URL:            "https://www.noblego.de/diesel-crucible-toro-zigarren/",
 			OriginCountry:  "Nicaragua",
-			Format:         "Robusto",
-			Form:           "Rund",
+			Format:         "Toro",
+			Form:           "Boxpressed",
 			WrapperCountry: []string{"Ecuador"},
 			FillerCountry:  []string{"Nicaragua"},
 			Strength:       "Medium",
-			Price:          2.1,
+			Price:          11.5,
 			Diameter:       19.8,
-			Length:         127,
-			Aromas:         []string{"Süß", "Würzig", "Zedernholz"},
-		},
-		{
-			Name:           "Reposado Robusto",
-			URL:            "https://www.noblego.de/reposado-robusto/",
-			OriginCountry:  "Nicaragua",
-			Form:           "Rund",
-			WrapperCountry: []string{"Honduras"},
-			FillerCountry:  []string{"Honduras", "Nicaragua"},
-			Strength:       "Medium",
-			Price:          2.1,
-			Diameter:       19.8,
-			Length:         127,
-			Aromas:         []string{"Cremig", "Gras", "Holz", "Leder", "Nuss", "Süß"},
+			Length:         152,
+			Aromas:         []string{"Espresso", "Nougat", "Nuss", "Röstaromen", "Schokolade", "Schwarzer Pfeffer"},
+			include:        true,
 		},
 	}
-	got := readList(list)
-	assert.Len(t, got, 2)
+	got, err, warn := readList(bytes.NewReader(list))
+	assert.NoError(t, err)
+	assert.ErrorContains(t, warn, "no required data found")
+	assert.Len(t, got, 1)
+	assert.Equal(t, want, got)
+}
+
+func Test_readSpanChildrenValue(t *testing.T) {
+	doc := strings.NewReader(`<span class="product-attribute-cig_diameter" title="Durchmesser in mm">
+	<span class="label">Ø</span>
+	<span class="value">19.8</span>
+</span>`)
+	node, err := html.Parse(doc)
+	assert.NoError(t, err)
+	want := "19.8"
+	assert.Equal(t, want, readSpanChildrenValue(node))
+}
+
+func Test_readUrlAndName(t *testing.T) {
+	doc := strings.NewReader(`<h2 class="product-name">
+	<a href="https://www.noblego.de/reposado-estate-blend-colorado-robusto-zigarren/" title="Reposado Estate Blend Colorado Robusto">
+	Reposado Estate Blend Colorado Robusto</a>
+</h2>`)
+	node, err := html.Parse(doc)
+	assert.NoError(t, err)
+	want := Record{
+		URL:  "https://www.noblego.de/reposado-estate-blend-colorado-robusto-zigarren/",
+		Name: "Reposado Estate Blend Colorado Robusto",
+	}
+	got := Record{}
+	readUrlAndName(node, &got)
+	assert.Equal(t, want, got)
+}
+
+func Test_readListProductDetails(t *testing.T) {
+	doc := strings.NewReader(`<div class="product-attributes">
+	<ul>
+		<li class="product-attribute-herkunft">
+			<span class="label">Herkunft</span>
+			<span class="data">                            <a href="https://www.noblego.de/zigarren-nicaragua/">Nicaragua</a>
+		</span>
+		</li>
+		<li class="product-attribute-cig_duration">
+			<span class="label">Rauchdauer</span>
+			<span class="data">45 bis 60 Min</span>
+		</li>
+		<li class="product-attribute-cig_size">
+			<span class="label">Format</span>
+			<span class="data">                            <a href="https://www.noblego.de/robusto-zigarren/">Robusto</a>
+		</span>
+		</li>
+		<li class="product-attribute-cig_wrapper_origin">
+			<span class="label">Deckblattherkunft</span>
+			<span class="data">Ecuador</span>
+		</li>
+		<li class="product-attribute-cig_filler">
+			<span class="label">Einlage</span>
+			<span class="data">Nicaragua</span>
+		</li>
+		<li class="product-attribute-cig_aroma">
+			<span class="label">Aroma</span>
+			<span class="data">Süß, Würzig, Zedernholz</span>
+		</li>
+		<li class="product-attribute-cig_form">
+			<span class="label">Form</span>
+			<span class="data">Rund</span>
+		</li>
+</ul>
+</div>`)
+	node, err := html.Parse(doc)
+	assert.NoError(t, err)
+	want := Record{
+		OriginCountry:  "Nicaragua",
+		Format:         "Robusto",
+		Form:           "Rund",
+		WrapperCountry: []string{"Ecuador"},
+		FillerCountry:  []string{"Nicaragua"},
+		Aromas:         []string{"Süß", "Würzig", "Zedernholz"},
+	}
+	got := Record{}
+	readListProductDetails(node, &got)
+	assert.Equal(t, want, got)
+}
+
+func Test_readListProductPrice(t *testing.T) {
+	doc := strings.NewReader(`<div class="add-to-cart">
+												<form action="https://www.noblego.de/checkout/cart/add/" method="post">
+													<ul class="product-prices">
+														<li>
+<span class="product-price-option-img">
+	<img src="https://www.noblego.de/media/catalog/product/cache/2/small_image/89x/9df78eab33525d08d6e5fb8d27136e95/r/e/reposado_estate_blend_colorado_robusto_01_1.jpg" data-dest-src="https://www.noblego.de/media/catalog/product/cache/2/small_image/363x/9df78eab33525d08d6e5fb8d27136e95/r/e/reposado_estate_blend_colorado_robusto_01_1.jpg">
+</span>
+															<span class="product-price-option-and-availability">
+					<span class="product-price-option-text product-price-option-type-cig_package_size">
+		<span title="Verpackungseinheit">10er</span>
+		<span class="availability-icon availability-suppliable-instock" title="Auf Lager und versandfertig "></span>
+	</span>
+	<span class="availability">
+		<span class="availability-text">Auf Lager und versandfertig </span>
+	</span>
+</span>
+
+															<span class="product-price">
+	<span class="price">20,37&nbsp;€</span>                                                                                <span class="product-price-option-discount">inkl. 3% Rabatt</span>
+													</span>
+
+															<div class="qty-wrapper">
+																<input type="text" class="input-text qty" name="qty[18555]">
+																<div class="qty-buttons-wrapper">
+																	<div class="qty-button increase"></div>
+																	<div class="qty-button decrease"></div>
+																</div>
+															</div>
+														</li>
+														<li>
+<span class="product-price-option-img">
+	<img src="https://www.noblego.de/media/catalog/product/cache/2/small_image/89x/9df78eab33525d08d6e5fb8d27136e95/r/e/reposado_estate_blend_colorado_robusto_05_1.jpg" data-dest-src="https://www.noblego.de/media/catalog/product/cache/2/small_image/363x/9df78eab33525d08d6e5fb8d27136e95/r/e/reposado_estate_blend_colorado_robusto_05_1.jpg">
+</span>
+															<span class="product-price-option-and-availability">
+					<span class="product-price-option-text product-price-option-type-cig_package_size">
+		<span title="Verpackungseinheit">Einzeln</span>
+		<span class="availability-icon availability-suppliable-instock" title="Auf Lager und versandfertig "></span>
+	</span>
+	<span class="availability">
+		<span class="availability-text">Auf Lager und versandfertig </span>
+	</span>
+</span>
+
+															<span class="product-price">
+	<span class="price">2,10&nbsp;€</span>                                            </span>
+
+															<div class="qty-wrapper">
+																<input type="text" class="input-text qty" name="qty[18556]">
+																<div class="qty-buttons-wrapper">
+																	<div class="qty-button increase"></div>
+																	<div class="qty-button decrease"></div>
+																</div>
+															</div>
+														</li>
+													</ul>
+													<div class="add-to-box">
+														<p class="validation-advice" style="display: none">Bitte geben Sie oben an, welche Menge Sie bestellen möchten!</p>
+														<div class="add-to-box-text">Preise inkl. 19% MwSt zzgl. Versandkosten. <b>Versandkostenfrei mit DHL</b> ab 60€ Warenwert!</div>
+														<button type="submit" title="In den Warenkorb" class="button btn-cart">
+		<span>
+			<span class="icon ic ic-cart"></span>
+			<span>In den Warenkorb</span>
+		</span>
+	</button>
+	</div></form></div>`)
+	node, err := html.Parse(doc)
+	assert.NoError(t, err)
+	want := Record{Price: 2.1, include: true}
+	got := Record{}
+	readListProductPrice(node, &got)
+	assert.Equal(t, want, got)
+}
+
+//go:embed details.html
+var details []byte
+
+func Test_readDetails(t *testing.T) {
+	want := Record{
+		Brand:           "Diesel",
+		Collection:      "Crucible",
+		Blender:         "AJ Fernandez",
+		FillingType:     "Longfiller",
+		WrapperType:     "Ecuador",
+		BinderCountry:   []string{"Ecuador"},
+		Gauge:           50,
+		Aroma:           "Medium-aromatisch",
+		LimitedEdition:  true,
+		SmokingDuration: "60 bis 90 Min",
+	}
+	got := Record{}
+	assert.NoError(t, readDetails(bytes.NewReader(details), &got))
 	assert.Equal(t, want, got)
 }
