205cd9b88a601f086da3e0af6973890086de6daf
Dmitry Kisler <admin@dkisler.com>
2025-05-04 12:33:44 +0100

    add adapter to the data from www.cigarcentury.com
    Signed-off-by: Dmitry Kisler <admin@dkisler.com>

--- extract/cigarcentury/cigarcentury.go
+++ extract/cigarcentury/cigarcentury.go
@@ -7,6 +7,7 @@ import (
 	"context"
 	"io"
 	"log/slog"
+	"strconv"
 	"strings"
 
 	"golang.org/x/net/html"
@@ -25,13 +26,15 @@ func (c Client) Read(ctx context.Context, id string) (r storage.Record, err erro
 		var doc *html.Node
 		if doc, err = html.Parse(v); err == nil {
 			n := htmlfilter.Node{Node: doc}
+			var categories = make([]string, 0)
+			var values = make([]*html.Node, 0)
 			for el := range n.Find("div.col-12.dato") {
 				for ch := range el.ChildNodes() {
 					if ch.DataAtom == atom.Div {
 						for _, att := range ch.Attr {
 							if att.Key == "class" {
-								switch att.Val {
-								case "valor descripcion":
+								switch {
+								case att.Val == "valor descripcion":
 									chS := htmlfilter.Node{Node: ch}
 									var s = make([]string, 0)
 									for el := range chS.Find("div.d-none") {
@@ -41,18 +44,155 @@ func (c Client) Read(ctx context.Context, id string) (r storage.Record, err erro
 									note := strings.Join(s, " ")
 									r.AdditionalNotes = &note
 
-								case "nombre":
+								case att.Val == "nombre":
+									categories = append(categories, ch.FirstChild.Data)
 
+								case strings.HasSuffix(att.Val, "strength-rating-show"):
+									values = append(values, ch)
+
+								case strings.HasPrefix(att.Val, "valor"):
+									values = append(values, ch.FirstChild)
 								}
 							}
 						}
 					}
 				}
 			}
+			err = augmentByCategoryValues(&r, categories, values)
 		}
 		return o, err
 	})
 	return r, err
+}
+
+func augmentByCategoryValues(r *storage.Record, categories []string, values []*html.Node) error {
+	var err error
+	for i, category := range categories {
+		switch category {
+		case "Origin":
+			r.ManufactureOrigin = strings.TrimSpace(values[i].Data)
+
+		case "Brand":
+			b := htmlfilter.Node{values[i]}
+			var tmp = make([]string, 0)
+			for el := range b.Find("li") {
+				for c := range el.ChildNodes() {
+					if c.DataAtom == atom.A {
+						tmp = append(tmp, strings.TrimSpace(c.LastChild.Data))
+					}
+				}
+			}
+			r.Brand = strings.Join(tmp, ", ")
+
+		case "Manufacturer":
+			v := strings.TrimSpace(values[i].Data)
+			r.Maker = &v
+
+		case "Wrapper":
+			if v := readSeparatedValues(values[i], ","); len(v) > 0 {
+				r.WrapperOrigin = v
+			}
+
+		case "Binder":
+			if v := readSeparatedValues(values[i], ","); len(v) > 0 {
+				r.BinderOrigin = v
+			}
+
+		case "Filler":
+			if v := readSeparatedValues(values[i], ","); len(v) > 0 {
+				r.FillerOrigin = v
+			}
+
+		case "Vitola":
+			r.Format = strings.TrimSpace(values[i].Data)
+
+		case "Box-Pressed":
+			switch v := strings.ToLower(strings.TrimSpace(values[i].Data)); v {
+			case "yes":
+				vv := true
+				r.IsBoxpressed = &vv
+			case "no":
+				vv := false
+				r.IsBoxpressed = &vv
+			}
+
+		case "Strength":
+			n := htmlfilter.Node{Node: values[i]}
+			for el := range n.Find("input") {
+				for _, att := range el.Attr {
+					if att.Key == "title" {
+						v := strings.TrimSpace(att.Val)
+						r.Strength = &v
+						break
+					}
+				}
+				break
+			}
+
+		case "Flavors":
+			v := readSeparatedValues(values[i], ",")
+			o := storage.AromaProfileCommunity{
+				Weights: make(map[string]float64),
+			}
+			// I decided to exclude the total number of voters because it does not appear reliable
+			// for the flavours votes
+			var totalCnt int
+			for _, vv := range v {
+				els := strings.Split(vv, "(")
+				flavour := strings.TrimSpace(els[0])
+				votes := readVotes(els[1])
+				totalCnt += votes
+				o.Weights[flavour] += float64(votes)
+			}
+			for flavour, votes := range o.Weights {
+				o.Weights[flavour] = votes / float64(totalCnt)
+			}
+			r.AromaProfileCommunity = &o
+
+		case "Length":
+			els := strings.Split(values[i].Data, " (")
+			if len(els) == 2 {
+				r.Length, _ = strconv.ParseFloat(strings.TrimSpace(strings.Split(els[1], "mm")[0]), 64)
+			}
+
+		case "Ring Gauge":
+			r.Ring, _ = strconv.ParseFloat(strings.TrimSpace(values[i].Data), 64)
+
+		case "Color":
+			v := strings.TrimSpace(values[i].Data)
+			r.Color = &v
+
+			// case "Specialized Ratings":
+			// 	r.SpecializedRatings
+		}
+	}
+	return err
+}
+
+// expected input ({{.int}})
+func readVotes(s string) int {
+	var tmp = make([]rune, 0, len(s))
+	for _, el := range strings.TrimSpace(s) {
+		switch el {
+		case '(', ')':
+		default:
+			tmp = append(tmp, el)
+		}
+	}
+	o, _ := strconv.ParseInt(string(tmp), 10, 64)
+	return int(o)
+}
+
+func readSeparatedValues(v *html.Node, delimiter string) []string {
+	tmp := strings.Split(v.Data, delimiter)
+	var o = make([]string, 0)
+	for _, vv := range tmp {
+		el := strings.TrimSpace(vv)
+		if el != "-" && el != "" {
+			o = append(o, el)
+		}
+	}
+	return o
 }
 
 func flipArr(s []string) []string {
--- extract/cigarcentury/cigarcentury_test.go
+++ extract/cigarcentury/cigarcentury_test.go
@@ -1,17 +1,111 @@
 package cigarcentury
 
 import (
+	"bytes"
+	"cigarsdb/extract"
 	"context"
-	"net/http"
+	_ "embed"
+	"io"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 )
 
-func TestClient_Read(t *testing.T) {
-	t.Skip("manual test")
-	c := Client{HTTPClient: http.DefaultClient}
-	r, err := c.Read(context.TODO(), "https://www.cigarcentury.com/en/cigars/arturo-fuente-casa-cuba-divine-inspiration")
+//go:embed fixtures/arturo-fuente-casa-cuba-divine-inspiration.html
+var ArturoFuenteCasaCubaDivineInspiration []byte
+
+func TestClient_ReadArturoFuenteCasaCubaDivineInspiration(t *testing.T) {
+
+	c := Client{
+		HTTPClient: &extract.MockHTTP{
+			Body: io.NopCloser(bytes.NewReader(ArturoFuenteCasaCubaDivineInspiration)),
+		},
+	}
+
+	wantURL := "https://www.cigarcentury.com/en/cigars/arturo-fuente-casa-cuba-divine-inspiration"
+	got, err := c.Read(context.TODO(), wantURL)
 	assert.NoError(t, err)
-	assert.NotEmpty(t, r.AdditionalNotes)
+
+	t.Parallel()
+
+	t.Run("origin", func(t *testing.T) {
+		assert.Equal(t, []string{"Dominican Republic"}, got.FillerOrigin)
+	})
+
+	t.Run("manufacturer", func(t *testing.T) {
+		assert.Equal(t, "Tabacalera Fuente", *got.Maker)
+	})
+
+	t.Run("brand", func(t *testing.T) {
+		assert.Equal(t, "Arturo Fuente, Casa Cuba", got.Brand)
+	})
+
+	t.Run("wrapper", func(t *testing.T) {
+		assert.Equal(t, []string{"Ecuadorian"}, got.WrapperOrigin)
+	})
+
+	t.Run("binder", func(t *testing.T) {
+		assert.Equal(t, []string{"Dominican Republic"}, got.BinderOrigin)
+	})
+
+	t.Run("filler", func(t *testing.T) {
+		assert.Equal(t, []string{"Dominican Republic"}, got.FillerOrigin)
+	})
+
+	t.Run("format", func(t *testing.T) {
+		assert.Equal(t, "Corona Grande", got.Format)
+	})
+
+	t.Run("box-pressed", func(t *testing.T) {
+		assert.False(t, *got.IsBoxpressed)
+	})
+
+	t.Run("strength", func(t *testing.T) {
+		assert.Equal(t, "Medium", *got.Strength)
+	})
+
+	t.Run("flavours", func(t *testing.T) {
+		flavours := *got.AromaProfileCommunity
+		assert.Empty(t, flavours.NumberOfVotes)
+
+		const wantWeight = float64(1. / 5)
+		wantWeights := map[string]float64{
+			"Oak":          wantWeight,
+			"Raisin":       wantWeight,
+			"Spices":       wantWeight,
+			"Coffee Beans": wantWeight,
+			"White Pepper": wantWeight,
+		}
+		assert.Equal(t, wantWeights, flavours.Weights)
+	})
+
+	t.Run("length", func(t *testing.T) {
+		assert.Equal(t, 156., got.Length)
+		assert.Empty(t, got.LengthInch)
+	})
+
+	t.Run("ring gauge", func(t *testing.T) {
+		assert.Equal(t, float64(47), got.Ring)
+		assert.Empty(t, got.Diameter)
+	})
+
+	t.Run("colour", func(t *testing.T) {
+		assert.Equal(t, "Colorado Maduro", *got.Color)
+	})
+}
+
+func Test_readVotes(t *testing.T) {
+	tests := map[string]int{
+		"(1)":     1,
+		" (1) ":   1,
+		"(1) ":    1,
+		"(1)\n":   1,
+		"(1)\n\t": 1,
+		"(1)\t":   1,
+	}
+	for in, want := range tests {
+		t.Run(in, func(t *testing.T) {
+			assert.Equalf(t, want, readVotes(in), "readVotes(%v)", in)
+		})
+	}
 }
